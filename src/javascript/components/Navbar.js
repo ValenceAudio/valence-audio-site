d4bfc47f01ba15477aee96f86086d68600d81a7e
Mark Johansson <valence.editing@gmail.com>
2017-01-31 13:36:38 -0500

    Fix broken link with logo

--- src/javascript/components/Navbar.js
+++ src/javascript/components/Navbar.js
@@ -1,28 +1,25 @@
 import React from 'react'
-import navbarLogo from '../../image/logo-loose.svg'
+import navbarLogo from '../../image/yellow-logo.png'
 import '../../scss/components/Navbar.scss'
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar navbar-toggleable-md navbar-light Navbar">
+    <nav className="navbar navbar-toggleable-md navbar-light navbar-custom">
     <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
     </button>
-    <a className="navbar-brand " href="#">Navbar</a>
+    <a className="navbar-brand " href="#"> <img src={navbarLogo} className="img-fluild" width="40" height="40" alt="va"/></a>
     <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-        <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
-        </li>
+        <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <a className="nav-link" href="#">Home </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+            </li>
         </ul>
     </div>
     </nav>
