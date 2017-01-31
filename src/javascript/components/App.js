ad72adf74efbb0b782b0ff5c5dfd1a08e57dff27
Mark Johansson <valence.editing@gmail.com>
2017-01-31 12:33:20 -0500

    Begin implementing main page and navbar

--- src/javascript/components/App.js
+++ src/javascript/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 import Mark from './Mark'
 const App = (props) => {
   return (
     <div className="container">
+    <Navbar/>
     <Mark/>
     </div>
   )
---
+++ src/javascript/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import navbarLogo from '../../image/logo-loose.svg'
+import '../../scss/components/Navbar.scss'
+
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-toggleable-md navbar-light Navbar">
+    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+    </button>
+    <a className="navbar-brand " href="#">Navbar</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+        <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link disabled" href="#">Disabled</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar;
