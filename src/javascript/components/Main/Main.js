7aded7c6498c5883c04a163f7775b75b1763c27d
Mark Johansson <valence.editing@gmail.com>
2017-02-22 22:20:31 -0500

    Modify services section to a vertical flow

--- src/javascript/components/Main/Main.js
+++ src/javascript/components/Main/Main.js
@@ -39,29 +39,61 @@ const Main = (props) => {
                       <hr className="primary"/>
                   </div>
               </div>
-              <div className="row text-center">
-                  <div className="col-md-4">
-                      <span className="fa-stack fa-4x">
-                          <i className="fa fa-circle fa-stack-2x service-icon"></i>
-                          <i className="fa fa-podcast fa-stack-1x fa-inverse"></i>
-                      </span>
-                      <h4 className="service-heading">Editing</h4>
-                      <p>Lorem ipsum dolor sit amet</p>
-                  </div>
-                  <div className="col-md-4">
-                      <span className="fa-stack fa-4x">
-                          <i className="fa fa-laptop fa-stack-1x fa-inverse service-icon"></i>
-                      </span>
-                      <h4 className="service-heading">Transcription</h4>
-                      <p>Lorem ipsum dolor sit amet</p>
-                  </div>
-                  <div className="col-md-4">
-                      <span className="fa-stack fa-4x">
-                          <i className="fa fa-cloud-upload fa-stack-1x fa-inverse service-icon"></i>
-                      </span>
-                      <h4 className="service-heading">Upload</h4>
-                      <p>Lorem ipsum dolor sit amet</p>
-                  </div>
+              <div className="row text-center icon-padding">
+                <div className="col-md-2"/>
+                <div className="col-md-2">
+                  <h2> Record Your Podcast </h2>
+                  <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. </p>
+                </div>
+                <div className="col-md-4">
+                    <span className="fa-stack fa-4x">
+                        <i className="fa fa-circle fa-stack-2x service-icon"></i>
+                        <i className="fa fa-microphone fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+              </div>
+              <div className="row text-center icon-padding">
+                <div className="col-md-4"/>
+                <div className="col-md-4">
+                    <span className="fa-stack fa-4x">
+                        <i className="fa fa-arrow-down fa-stack-1x fa-inverse service-icon"></i>
+                    </span>
+                </div>
+              </div>
+              <div className="row text-center icon-padding">
+                <div className="col-md-4"/>
+                <div className="col-md-4">
+                    <span className="fa-stack fa-4x">
+                        <i className="fa fa-circle fa-stack-2x service-icon"></i>
+                        <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div className="col-md-2">
+                  <h2> Record Your Podcast </h2>
+                  <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. </p>
+                </div>
+                <div className="col-md-2"/>
+              </div>
+              <div className="row text-center icon-padding">
+                <div className="col-md-4"/>
+                <div className="col-md-4">
+                    <span className="fa-stack fa-4x">
+                        <i className="fa fa-arrow-down fa-stack-1x fa-inverse service-icon"></i>
+                    </span>
+                </div>
+              </div>
+              <div className="row text-center icon-padding">
+                <div className="col-md-2"/>
+                <div className="col-md-2">
+                  <h2> Record Your Podcast </h2>
+                  <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. </p>
+                </div>
+                <div className="col-md-4">
+                    <span className="fa-stack fa-4x">
+                        <i className="fa fa-circle fa-stack-2x service-icon"></i>
+                        <i className="fa fa-podcast fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
               </div>
             </div>
           </div>
