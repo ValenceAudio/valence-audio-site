7246cf0d0b59c4f714a8d0584cbff90983c7d6fe
Mackenzie Drew <mackiedrew@gmail.com>
2017-04-14 10:40:48 -0400

    :fire: Starting to refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './javascript/components/App/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './javascript/components/App/App';
 import './javascript/components/App/App.scss';
 
 ReactDOM.render(
